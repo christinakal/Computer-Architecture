2c4c6be50474817a27a7905eb8d81c56351b223f
Christina Kaliora <develop@pirron.one>
2020-07-22 13:22:35 +0200

    still not working - started refactoring load() and changing the login in run()

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -16,18 +16,20 @@ class CPU:
 
         address = 0
 
-        # For now, we've just hardcoded a program:
+        program = []
+        try:
+            with open(sys.argv[1]) as f:
+                for line in f:
+                    program.append(line)
+            print(1) # is this like print(true)?
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        except FileNotFoundError:
+            print(f"{sys.argv[0]}: {sys.argv[1]} not found!")
+            sys.exit(2) # what this means?
 
+        if program is None:
+            print("No program specified!")
+            return
         for instruction in program:
             self.ram[address] = instruction
             address += 1
@@ -91,7 +93,6 @@ class CPU:
                 self.pc += 2
             elif instruction == 0b00000001: # HALT (HLT)
                 running = False
-            else:
-                print(f"Unknown instruction {instruction}")
+
 
 
